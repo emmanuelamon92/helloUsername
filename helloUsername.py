f6cc031d832bc6b8b3139ce6eaea49783390036c
Emmanuel Amon <emmanuelamon92@gmail.com>
2021-01-30 08:35:31 -0500

    first commit

---
+++ helloUsername.py
@@ -0,0 +1,34 @@
+# # Question 1
+# def hello_name (user_name):
+#     print ('hello_' + user_name)
+
+# # hello_name("Eman")
+
+# # Question 2
+# for odd in range(1, 100):
+#     if odd % 2 == 1:
+#         print (odd)
+
+# # Question 3
+# def max_num_in_list(a_list):
+#     #userInput = []
+#     #for a_list in userInput:
+#     #print (max(a_list), a_list)
+#     return (max(a_list))
+        
+# # max_num_in_list([9, 4, 46, 23, 45, 56, 21])
+
+# # Question 4
+# def is_leap_year(a_year):
+#     if a_year % 4 == 0 & a_year % 400 == 0:
+#         #print ('Leap Year!')
+#         return True
+#     else:
+#         #print ('No Leap')
+#         return False
+
+# # is_leap_year(2017)
+
+# # Question 5
+def is_consecutive(a_list):
+    
